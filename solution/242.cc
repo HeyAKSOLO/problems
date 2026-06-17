b8da0e2f5a5123a060ac08ba37f6d475b30622b6
HeyAK <heykmorph@gmail.com>
2026-06-17 07:29:49 +0530

    add solution of valid anagram and 3936

---
+++ solution/242.cc
@@ -0,0 +1,42 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+// 242 - Valid Anagram
+bool anagram(string s , string t){
+    if(s.length() == t.length()){       // compare lengths
+        unordered_map<char, int> mpp;
+        for(char i : s){            // store each char frequency in map
+            mpp[i]++;
+        }
+        for(char j : t){            // subtract each char frequency
+            mpp[j]--;
+        }
+        for(auto &k : mpp){         // if any value is != 0, not anagram
+            if(k.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+
+// better
+bool anagram(string s , string t){
+    if(s.length() == t.length()){       // compare lengths
+        int mpp[26];
+        for(int i = 0; i < s.length(); i++){
+            mpp[s[i] - 'a']++;
+            mpp[t[i] - 'a']--;
+        }
+        for(auto &k : mpp){         // if any value is != 0, not anagram
+            if(k != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
---
+++ solution/3936.cc
@@ -0,0 +1,20 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+// 3936 - Minimum swaps needed to move zeroes to end
+int minimumSwaps(vector<int>& nums) {
+    int i = 0;
+    int j = nums.size() - 1;
+    int count = 0;
+    while(i < j){
+        while(i < j && nums[i] != 0) i++; 
+        while(j > i && nums[j] == 0) j--;
+        if(i < j){
+            // swap(nums[i], nums[j]); -- they're saying minimum swaps needed to telling to actually swap 
+            i++;
+            j--;
+            count++;
+        }
+    }
+    return count;
+}
