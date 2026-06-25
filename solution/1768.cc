a8a5d767222f469e22de2ebc9b02135bc8fab7df
HeyAK <heykmorph@gmail.com>
2026-06-25 19:24:46 +0530

    add solution 1768

---
+++ solution/1768.cc
@@ -0,0 +1,21 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+// Merge Strings Alternately
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string merged;
+        int i = 0;
+        while(i < word1.length() || i < word2.length()){
+            if(i < word1.length()){
+                merged.push_back(word1[i]);
+            }
+            if(i < word2.length()){
+                merged.push_back(word2[i]);
+            }
+            i++;
+        }
+        return merged;
+    }
+};
