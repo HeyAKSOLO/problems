c501cbe328aed7fd34481b4b69f2ef0695bf02d9
HeyAK <heykmorph@gmail.com>
2026-06-27 19:16:39 +0530

    add solution 796

---
+++ solution/796.cc
@@ -0,0 +1,27 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+// Rotate String
+
+// My approach(brute force)
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length() == goal.length()){
+            int n = s.length();
+            for(int i = 0; i < n; i++){
+                char temp = s[0];
+                for(int j = 0; j < n-1; j++){
+                    s[j] = s[j+1];
+                }
+                s[n-1] = temp;
+                if(s == goal){
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+};
+
