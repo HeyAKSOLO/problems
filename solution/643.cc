bfca30664d98b1563478887c0555d9c1f4b1bdce
heyAK <heykmorph@gmail.com>
2026-07-05 19:43:49 +0530

    add solution 643

---
+++ solution/643.cc
@@ -0,0 +1,19 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+// Maximum Average Subarray I
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int sum = 0;
+        for(int start = 0; start < k; start++){
+            sum += nums[start];
+        }
+        int maxm = sum;
+        for(int i = k; i < nums.size(); i++){
+            sum += nums[i] - nums[i-k];
+            maxm = max(maxm, sum);
+        }
+        return (double)maxm / k;
+    }
+};
