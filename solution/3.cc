aa0261b96c8523aac7e1eec4cf0c73309106ff2b
heyAK <heykmorph@gmail.com>
2026-07-06 23:27:03 +0530

    add solution 3

---
+++ solution/3.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+//
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int left = 0;
+        int max_size = 0;
+        unordered_set<int> u_set;
+        for(int i = 0; i < s.length(); i++){
+            while(u_set.find(s[i]) != u_set.end()){ // till the duplicate element is removed
+                u_set.erase(s[left]);               // from the set erase it
+                left++;             // increase position of left (last duplicate elem)
+            }
+            u_set.insert(s[i]);     // insert current elem
+            max_size = max(max_size, i - left + 1); // each time check the size of window 
+        }                                           // with previous one
+        return max_size;
+    }
+};
