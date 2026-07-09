27858b9b77c3677a55080db3ab8436a1b025bb8b
heyAK <heykmorph@gmail.com>
2026-07-09 23:14:47 +0530

    add solution 1207

---
+++ solution/1207.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+// Unique number of Occurrences
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> freq;
+        unordered_set<int> occur;
+        for(int i : arr){
+            freq[i]++;
+        }
+        for(auto &i : freq){
+            if(occur.count(i.second)) return false;
+            occur.insert(i.second);
+        }
+        return true;
+    }
+};
