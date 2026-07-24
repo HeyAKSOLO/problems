f5e9ec171baa2d556369e117bc8f759bafaadfd6
heyAK <heykmorph@gmail.com>
2026-07-24 19:50:06 +0530

    add solution 342

---
+++ solution/342.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// power of 4
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n == 0) return false;
+        while(n % 4 == 0){
+            n /= 4;
+        }
+        return n == 1;
+    }
+};
